10d9af62ce614e8a7a26b29e3ccd24dcc5eda67d
Manju145 <manjuyadav0514001@gmail.com>
2023-03-30 22:29:29 +0530

    project

---
+++ amazonn/src/adminpages/Login.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+       <div className='loginform'>
+      <br/>
+    <h2 className='logname'>User Login</h2>
+ 
+     <form >
+     <br/>
+
+        <input className='inputform' type="name" placeholder='User Name'/>
+
+      <br/>
+      <br/>
+       
+        <input className='inputform' type="email" placeholder='Email'/>
+        <br/>
+    <br/>
+        <input  className='inputform' type="password" placeholder='Password'
+    
+        ></input>
+        <br/>
+        <br/>
+        <button  className='btnform' type='submit' >Log In</button>
+    </form>
+
+    
+    </div>
+    </div>
+  )
+}
+
+export default Login
+
+
