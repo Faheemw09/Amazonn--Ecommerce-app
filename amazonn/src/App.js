f0846bc50c4bb0a15c793798d902ccd38318d137
Manju145 <manjuyadav0514001@gmail.com>
2023-03-30 23:37:52 +0530

    pro

--- amazonn/src/App.js
+++ amazonn/src/App.js
@@ -1,9 +1,8 @@
 import './App.css';
- HEAD
-import Home from './Components/Home';
+// import Home from './Components/Home';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css"
-import {Admin} from "./adminpages/Admin"
+// import {Admin} from "./adminpages/Admin"
 import Footer from './components/Footer';
 import { Navbar } from './components/Navbar';
 import { MainRoute } from './Pages/MainRoute';
@@ -11,12 +10,13 @@ import { MainRoute } from './Pages/MainRoute';
 
 
 function App() {
+
   return (
     <div className="App">
-     <Home/>
-   <Admin/>
+     {/* <Home/>
+   <Admin/> */}
       <Navbar/>
-      <MainRoute/>
+      <MainRoute />
       <Footer/>
 
     </div>
--- amazonn/src/Pages/MainRoute.jsx
+++ amazonn/src/Pages/MainRoute.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import Login from "../adminpages/Login";
 import HomePage from "./HomePage";
 
 export const MainRoute = () => {
     return (
         <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route  path="/login" element={<Login/>}/>
             <Route path="*" element={<h4>404 Page not Found</h4>} />
         </Routes>
     )
